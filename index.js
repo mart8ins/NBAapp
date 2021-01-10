0c7d7d28d47c77cde8ef594ec6126595b0913bc6
mart8ins <mart8ins@gmail.com>
2021-01-10 10:36:32 +0200

    Refactored layout using boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.engine("ejs", ejsMate);
 
 mongoose.connect("mongodb://localhost:27017/nbaStats", {
     useNewUrlParser: true,
